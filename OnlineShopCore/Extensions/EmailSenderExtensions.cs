94f774aa7bc21593ccd84bcdd959de2f019777b0
Nguyen Quang <16dh110159@st.huflit.edu.vn>
2019-03-22 15:02:01 +0700

    fix email sender link

--- OnlineShopCore/Extensions/EmailSenderExtensions.cs
+++ OnlineShopCore/Extensions/EmailSenderExtensions.cs
@@ -12,7 +12,7 @@ namespace OnlineShopCore.Services
     {
         public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
         {
-            string sHTML = File.ReadAllText(@"D:\OnlineShop with ASP. NET Core\OnlineShopCore\OnlineShopCore\wwwroot\templates\emailSendTemp.txt");
+            string sHTML = File.ReadAllText(@"..\OnlineShopCore\wwwroot\templates\emailSendTemp.txt");
             sHTML = sHTML.Replace("id=\"veryImportant\" href=\"#\""
                 , $"id=\"veryImportant\" href='{HtmlEncoder.Default.Encode(link)}' ");
             return emailSender.SendEmailAsync(email, "Welcome to CozaStore!!",
